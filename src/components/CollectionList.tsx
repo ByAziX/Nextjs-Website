bf3cc35a651583bf955a851f6d783fa027623140
Azix <Azix1@outlook.fr>
2024-02-04 01:42:27 +0100

    Responsive mobile

--- src/components/CollectionList.tsx
+++ src/components/CollectionList.tsx
@@ -28,7 +28,7 @@ const CollectionList: React.FC<CollectionListProps> = ({ collections,totalCount,
       <Heading as="h2" size="xl">
     {`Explore ${totalCount} Collections`}
   </Heading>
-        <SimpleGrid columns={{ base: 2, md: 6 }} spacing="4">
+        <SimpleGrid columns={{ base: 1, md: 6 }} spacing="4">
             {collections.map((collection) => <CollectionCard key={collection.collectionId} item={collection} />)}
         </SimpleGrid>
         <Pagination 
