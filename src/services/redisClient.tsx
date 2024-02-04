251dd8a682a7fd974e0a7c3000eb5629176d5dad
Azix <Azix1@outlook.fr>
2024-02-04 14:12:42 +0100

    build vercel with env

--- src/services/redisClient.tsx
+++ src/services/redisClient.tsx
@@ -16,19 +16,20 @@ class MemoryCache {
   }
 }
 
-
+/*
 const redisClient: RedisClientType = createClient({
   url: 'redis://localhost:6380'
 });
+*/
 
-/*
+
 const redisClient: RedisClientType = createClient({
   password: process.env.REDIS_PASSWORD,
   socket: {
       host: process.env.REDIS_HOST,
       port: 14911
   }
-});*/
+});
 
 const memoryCache = new MemoryCache();
 
